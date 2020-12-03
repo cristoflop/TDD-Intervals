f0cf2f05e5b0c06b27459118a1e13ff3fe3a8cbe
cristoflop <cristofer.lc.99@gmail.com>
2020-12-03 14:38:03 +0100

    Added test with null interval

--- src/test/java/usantatecla/IsIntersectedTest.java
+++ src/test/java/usantatecla/IsIntersectedTest.java
@@ -1,4 +1,22 @@
 package usantatecla;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+
 public class IsIntersectedTest {
+
+    private Interval interval;
+
+    @Before
+    public void before() {
+        this.interval = new IntervalBuilder().open(1).open(10).build();
+    }
+
+    @Test(expected = AssertionError.class)
+    public void testGivenIntervalWhenCheckIfIsIntersectedWithNullIntervalThenAssertionError() {
+        Interval nullInterval = null;
+        boolean isIntersected = this.interval.isIntersected(nullInterval);
+    }
+
 }
